ceb7dfaa9cd46b24f8c99c2b0b344df0c3858558
NikhilMore072 <nikhilmorej@gmail.com>
2024-09-04 15:16:50 +0530

    User Model namespace chnage

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Auth;
-use App\User;
+use App\Models\User;
 use App\Clinic_location;
 use App\Product_name;
 use App\ClinicOrders;
